863ec13975e0e85c61a47a6e4d986d67bf1d0aa8
AqibNiazi <aqibjaved5201@gmail.com>
2023-08-28 17:28:37 +0500

    index file removed from source

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const router = require("./collections");
+const router = require("./src/collections");
 require("dotenv").config();
 const app = express();
 
